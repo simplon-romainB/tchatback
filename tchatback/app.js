69d05934e460a7bf06b21f4a751c58881f72548d
simplon-romainB <neriaa@hotmail.fr>
2020-05-01 15:25:59 +0200

    ok

--- tchatback/app.js
+++ tchatback/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var registerRouter = require('./routes/register');
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/register', registerRouter);
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
